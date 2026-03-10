71989675dddd33742aef47dfdc38a373c4452474
Shaheer Ahmed <gettoknowshaheer798@gmail.com>
2026-03-10 07:26:05 +0500

    Fix: correctly load dotenv in server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 
 const app = express();
 app.use(bodyParser.json());
